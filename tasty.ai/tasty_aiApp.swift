b5504bfb86d516749cc90b7755591ffdf7894d88
Shinjan P <116586681+flaminshinjan@users.noreply.github.com>
2024-12-14 16:56:56 +0530

    Initial Commit

---
+++ tasty.ai/tasty_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasty_aiApp.swift
+//  tasty.ai
+//
+//  Created by Shinjan Patra on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct tasty_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tasty.aiTests/tasty_aiTests.swift
@@ -0,0 +1,17 @@
+//
+//  tasty_aiTests.swift
+//  tasty.aiTests
+//
+//  Created by Shinjan Patra on 14/12/24.
+//
+
+import Testing
+@testable import tasty_ai
+
+struct tasty_aiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
